de7123902f7751c75a10aa699fb4e6eca2bc302a
Ziad Hassan <ziadmhassan@aucegypt.edu>
2023-12-10 02:36:43 +0200

    fixed next.js.config

--- pages/mainpage/playground.tsx
+++ pages/mainpage/playground.tsx
@@ -5,6 +5,7 @@ import '../../src/app/globals.css';
 export default function Editor() {
     return(
         <div className='homepage'>
+            <ResponsiveAppBar/>
             <iframe className='code-editor-container-iframe'
                 src="https://www.interviewbit.com/embed/snippet/2eeda5a80dcf362d8494"
                 title="Interviewbit Ide snippet/2eeda5a80dcf362d8494"
